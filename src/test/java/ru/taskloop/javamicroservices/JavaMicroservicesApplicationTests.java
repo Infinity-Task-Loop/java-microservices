2aecab22174c9331d596c122abb7bc8f38722607
Vadim Subbota <diagsubb@gmail.com>
2025-10-20 23:22:36 +0300

    environment-setup

---
+++ src/test/java/ru/taskloop/javamicroservices/JavaMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.taskloop.javamicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
